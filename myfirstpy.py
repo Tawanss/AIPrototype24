694aa9fc840101904fcca63f0cbb7d822fd62525
Tawanss <tawan.b@kkumail.com>
2025-01-07 14:18:52 +0700

    HW1 done

--- myfirstpy.py
+++ myfirstpy.py
@@ -1,2 +1,43 @@
 import argparse
 from datetime import datetime
+
+def parse_input():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--bd',
+        type=str,
+        required=True,
+        help='Birthday of the user in YYYYMMDD format'
+    )
+
+    parser.add_argument(
+        '--name',
+        type=str,
+        default='tawan',
+        help='Input the name of the user'
+    )
+
+    args = parser.parse_args()
+    return args
+
+def printHello(who):
+    print(f"Hello World!, {who}!!")
+
+def cal_days_since_birthday(bd):
+    today = datetime.today()
+    birth_date = datetime.strptime(bd, '%Y%m%d')
+
+    if birth_date > today:
+        raise ValueError("The birthday cannot be in the future!")
+
+    delta = today - birth_date
+    return delta.days
+
+if __name__ == "__main__":
+    input_v = parse_input()
+    print('This is my first .py file.')
+    printHello(input_v.name)
+    days_since_birthday = cal_days_since_birthday(input_v.bd)
+    print(f'It has been {days_since_birthday} days since your birthday!')
+    
